3aa8b13ba0ee03dc0384b194d15c7f510ec66364
RogerTM <roger213tm@gmail.com>
2017-11-14 23:34:57 -0500

    Commit inicial

---
+++ shirka.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sleep 20s
+killall conky
+cd "/home/rogertm/Projects/ShirkaConky" &
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaLoading" &
+sleep 3s
+killall conky
+cd "/home/rogertm/Projects/ShirkaConky" &
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaWellcome" &
+sleep 5s
+killall conky
+sleep 2s
+cd "/home/rogertm/Projects/ShirkaConky" &
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaClock" &
+cd "/home/rogertm/Projects/ShirkaConky"
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaSystem" &
+cd "/home/rogertm/Projects/ShirkaConky"
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaInfo" &
+cd "/home/rogertm/Projects/ShirkaConky"
+conky -c "/home/rogertm/Projects/ShirkaConky/ShirkaAudacious" &
